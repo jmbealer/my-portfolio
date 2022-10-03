cf4af1911ddc9c92239815a08a6b202a39988987
Justin Bealer <jmbealer11@gmail.com>
2022-10-03 08:49:05 -0500

    updating portfolio

---
+++ public/project.js
@@ -0,0 +1,26 @@
+let projects = [
+  {
+    name: "project one",
+    tags: "#javascript, #fullstack, #ui/ux, #backend",
+    // image: "project (1).png",
+    image: "computer.jpeg",
+  },
+  {
+    name: "project two",
+    tags: "#javascript, #fullstack",
+    // image: "project (2).png",
+    image: "computer.jpeg",
+  },
+  {
+    name: "project three",
+    tags: "#ui/ux",
+    // image: "project (3).png",
+    image: "computer.jpeg",
+  },
+  {
+    name: "project four",
+    tags: "#ui/ux, #backend",
+    // image: "project (4).png",
+    image: "computer.jpeg",
+  },
+]
